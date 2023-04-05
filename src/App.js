071867da13d20366aee1ede4fceff3d25adb5c98
Clecio Batista Santos <cleciosan@hotmail.com>
2023-04-05 17:23:32 -0300

    Inicio da Implementação do MainDash

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import Sidebar from './components/SideBar/SideBar';
+import MainDash from './components/MainDash/MainDash';
 
 function App() {
   return (
     <div className="App">
         <div className="AppGlass">
           <Sidebar/>
+          <MainDash/>
         </div>
     </div>
   );
